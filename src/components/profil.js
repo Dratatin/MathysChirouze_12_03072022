9d1014d16e949a5fe190a0b1ff1d148c43f3806e
Dratatin <matchirouze@orange.fr>
2022-07-04 14:11:26 +0200

    install of react router and the first components

---
+++ src/components/profil.js
@@ -0,0 +1,7 @@
+const Profil = () => {
+    return (
+        <div>Yo</div>
+    )
+}
+
+export default Profil
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import './index.css';
+import Profil from './components/profil';
+import Header from './components/header';
+import Sidebar from './components/sidebar';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
+      <Header />
+      <Sidebar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Profil/>}/>
+        </Routes>
+      </main>
+    </BrowserRouter>
   </React.StrictMode>
 );
