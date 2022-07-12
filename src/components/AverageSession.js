d4ac9e98b280f9bed91cdf3ad00d262a4dc9a5b6
Dratatin <matchirouze@orange.fr>
2022-07-12 15:20:31 +0200

    average session component designed at  90%

--- src/components/AverageSession.js
+++ src/components/AverageSession.js
@@ -1,22 +1,45 @@
 import { LineChart, Line, XAxis, Tooltip, ResponsiveContainer } from 'recharts';
+import CustomTooltip from './customTooltip';
 
 const AverageSession = ({ averageSession }) => {
+    averageSession = averageSession.map(element => ({
+        ...element,
+        day : ["L","M","M","J","V","S","D"][element.day - 1]
+    }))
+
     return(
-        <div>
-            <ResponsiveContainer width="100%" height="100%">
+        <div className="average-session">
+            <h3 className="average-session__label">Durée moyenne de <br/> sessions</h3>
+            <ResponsiveContainer width="100%" height={180}>
                 <LineChart
                 width={500}
                 height={300}
                 data={averageSession}
                 margin={{ top: 10, right: 10, bottom: 10, left: 10 }}
                 >
+                    <defs>
+                        <linearGradient id="linear">
+                            <stop offset="0%" stopColor="white" stopOpacity={0.5} />
+                            <stop offset="100%" stopColor="white" stopOpacity={1} />
+                        </linearGradient>
+                    </defs>
                     <XAxis 
                     dataKey="day" 
                     tickLine={false} 
                     axisLine={false} 
+                    tick={{ fill: "rgba(255, 255, 255, 0.5)", fontFamily: "Roboto", fontWeight: 500, fontSize: 14 }} 
+                    height={50}
+                    tickSize={25}
                     />
-                    <Tooltip />
-                    <Line type="monotone" dataKey="sessionLength" stroke="#8884d8" dot={false} activeDot={{ r: 8 }} />
+                    <Tooltip content={<CustomTooltip />} cursor={{stroke: "none"}} />
+                    <Line 
+                        type="natural" 
+                        dataKey="sessionLength" 
+                        unit="min" 
+                        dot={false}         
+                        strokeWidth={2}
+                        stroke="url(#linear)"
+                    />
                 </LineChart>
             </ResponsiveContainer>
         </div>
--- src/components/customTooltip.js
+++ src/components/customTooltip.js
@@ -1,10 +1,10 @@
 const CustomTooltip = ({ active, payload }) => {
     if (active) {
         return (
-            <div className="tooltip">
+            <div className={payload[0].fill === "#fff" ? "tooltip tooltip--light" : "tooltip tooltip--primary"}>
                 <ul className="tooltip__listctn">
-                    <li>{payload[0].value}{payload[0].unit}</li>
-                    <li>{payload[1].value}{payload[1].unit}</li>
+                    {payload[0] ? (<li>{payload[0].value}{payload[0].unit}</li>) : null}
+                    {payload[1] ? (<li>{payload[1].value}{payload[1].unit}</li>) : null}
                 </ul>
             </div>
         )
