d56511d390ee41651ab576bf0df76671dd895523
Dratatin <matchirouze@orange.fr>
2022-07-28 17:03:17 +0200

    small style adjustment

--- src/layout/Sidebar.js
+++ src/layout/Sidebar.js
@@ -2,6 +2,7 @@ import zen from '../assets/zen.svg'
 import swimming from '../assets/swimming.svg'
 import bike from '../assets/bike.svg'
 import strength from '../assets/strength.svg'
+import { NavLink } from 'react-router-dom'
 
 /**
  * Component of Sidebar with physical activities
@@ -13,10 +14,10 @@ const Sidebar = () => {
     return (
         <div className="sidebar">
             <ul className="sidebar__listctn">
-                <li className="sidebar__listctn__element"><img src={zen} alt="zen"></img></li>
-                <li className="sidebar__listctn__element"><img src={swimming} alt="swimming"></img></li>
-                <li className="sidebar__listctn__element"><img src={bike} alt="bike"></img></li>
-                <li className="sidebar__listctn__element"><img src={strength} alt="strength"></img></li>
+                <li className="sidebar__listctn__element"><NavLink to="#"><img src={zen} alt="zen"></img></NavLink></li>
+                <li className="sidebar__listctn__element"><NavLink to="#"><img src={swimming} alt="swimming"></img></NavLink></li>
+                <li className="sidebar__listctn__element"><NavLink to="#"><img src={bike} alt="bike"></img></NavLink></li>
+                <li className="sidebar__listctn__element"><NavLink to="#"><img src={strength} alt="strength"></img></NavLink></li>
             </ul>
             <p className="sidebar__copiryght">Copiryght, SportSee 2020</p>
         </div>
